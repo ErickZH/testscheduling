b23ae34b65377649a8c51b40e70d2f1b1dec6ea7
ErickZH <zaraterick@outlook.com>
2018-01-03 15:41:42 -0600

    Modificando el seed.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 
+use DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\User', 10)->create();
+    	DB::table('users')->insert(array(
+    		array("name"=>"Claribel","email"=>"cpruce0@etsy.com","password"=>"0jwnGst89rRb"),
+    		array("name"=>"Gertruda","email"=>"goxtiby1@altervista.org","password"=>"Mk0HSv"),
+    		array("name"=>"Silvanus","email"=>"silson2@tripod.com","password"=>"9EgB0DO"),
+    		array("name"=>"Netta","email"=>"npuig3@woothemes.com","password"=>"ZZr1S3TRL"),
+    		array("name"=>"Renata","email"=>"rrohlfs4@virginia.edu","password"=>"bs76gdlgTO8"),
+    		array("name"=>"Packston","email"=>"psiemon5@va.gov","password"=>"chuKBtgbq6"),
+    		array("name"=>"Mia","email"=>"mbecket6@economist.com","password"=>"BbAOkxLncXl"),
+    		array("name"=>"Seline","email"=>"snoen7@nih.gov","password"=>"bIBTHg1"),
+    		array("name"=>"Garreth","email"=>"gtaffarello8@stanford.edu","password"=>"ayXSsqLpV"),
+    		array("name"=>"Jud","email"=>"jgedling9@ucoz.ru","password"=>"H8TvpF8Ogpw")
+    	));
     }
 }
